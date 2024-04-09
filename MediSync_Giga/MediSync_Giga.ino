43250e35bcc0d0699ccd18063d5fcfbbb08bb7bf
yidilozdemir <30597301+yidilozdemir@users.noreply.github.com>
2024-04-09 15:20:14 +0100

    Update MediSync_Giga.ino to recieve servoMotor trigger

--- MediSync_Giga/MediSync_Giga.ino
+++ MediSync_Giga/MediSync_Giga.ino
@@ -23,6 +23,16 @@ int selectedServo = 0;   // Variable to store the selected servo (1 or 2 or 3)
 HX711 scale;
 unsigned long buzzerStartTime = 0; // Variable to store the time when the weight last became non-zero
 bool buzzerActive = false; // Flag to track if the buzzer is currently active
+bool servoActivate = false;
+
+const char* ssid = "your_SSID";
+const char* password = "your_PASSWORD";
+int boxId;
+int tankId;
+const char* medicineName;
+const char* scheduledTime;
+const char* serverUrl = "https://your-azure-webapp.azurewebsites.net/medicine-reminder";
+const char* updateUrl = "https://your-azure-webapp.azurewebsites.net/updateMedicineTaken";
 
 void setup() {
   pinMode(BUTTON_PIN, INPUT);    // Set the button pin as input
@@ -37,6 +47,19 @@ void setup() {
   scale.tare(); //Assuming there is no weight on the scale at start up, reset the scale to 0
 
   Serial.begin(9600); // Initialize serial communication
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi...");
+  }
+  
+  Serial.println("Connected to WiFi");
+
+  HTTPClient http;
+  http.begin(serverUrl);
+  
 }
 
 void loop() {
@@ -62,9 +85,15 @@ void loop() {
     // Weight is not greater than 0.01 lbs
     // Check if the buzzer was active
     if (buzzerActive) {
+      selectedServo != -1 // the tank just dispensed 
       // Stop the buzzer
       noTone(BUZZER_PIN);
       buzzerActive = false;
+
+      //update database that the pill was dispensed
+      UpdateDatabase();
+      //update selectedServo back to -1, ie do not trigger
+      selectedServo = -1;
     }
   }
 
@@ -73,10 +102,17 @@ void loop() {
 
   // servos
 
-  if (Serial.available() > 0) { // Check if there are characters available
-    char input = Serial.read(); // Read the input from serial
-    if (input >= '1' && input <= '3') { // Check if the input is a valid servo number
-      selectedServo = input - '0'; // Convert character to integer
+  // servos
+
+  //read if there is a trigger action: if there is no trigger, it returns -1, if it read a signal, it returns the tankId ie servoMotor id
+  servoMotorId = handleMedicineReminder();
+  
+  if (servoMotorId != -1 && servoMotorId >= 1 && servoMotorId <= 3)  {
+  //if (Serial.available() > 0) { // Check if there are characters available
+  //  char input = Serial.read(); // Read the input from serial
+ //   if (input >= '1' && input <= '3') { // Check if the input is a valid servo number
+ //     selectedServo = input - '0'; // Convert character to integer
+      selectedServo = servoMotorId;
       Serial.print("Servo ");
       Serial.print(selectedServo);
       Serial.println(" selected. Press the button to trigger.");
@@ -142,5 +178,61 @@ void triggerServo(Servo servo) {
   }
 
   delay(1000);
+}
+
+
+int handleMedicineReminder() {
+
+  int httpResponseCode = http.GET();
+  
+  if (httpResponseCode == HTTP_CODE_OK) {
+    String response = http.getString();
+    
+    DynamicJsonDocument doc(1024);
+    DeserializationError error = deserializeJson(doc, response);
+    
+    if (error) {
+      Serial.println("No pills to be dispensed now");
+      return -1;
+    }
+    Serial.println("Pills to dispense in tank");
+    //boxId = doc["boxId"];
+    tankId = doc["tankId"];
+    Serial.println(tankId);
+    medicineName = doc["medicineName"];
+    scheduledTime = doc["scheduledTime"];
+    
+    return tankId;
+
+  } else {
+    Serial.print("HTTP error code: ");
+    Serial.println(httpResponseCode);
+    return -1
+  }
+  
+  http.end();
+}
+
+void UpdateDatabase(){
+  
+  String payload = "{\"boxId\":" + String(boxId) + ",\"tankId\":" + String(tankId) + ",\"medicineName\":" + String(medicineName) + ",\"scheduledTime\":" + String(scheduledTime) + "}";
+
+  Serial.println("Payload:");
+  Serial.println(payload);
+
+  String url = updateUrl;
+
+  http.addHeader("Content-Type", "application/json");
+  int httpResponseCode = http.POST(payload);
+  if (httpResponseCode > 0) {
+    String response = http.getString();
+    Serial.println("HTTP POST response:")
+    Serial.println(httpResponseCode);
+    Serial.println(response);
+  } else {
+    Serial.println("HTTP POST request failed");
+  }
+  http.end();
+
 }
 
